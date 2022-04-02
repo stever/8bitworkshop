6ece7086c88e68214df9361afb40072250eee5db
Steven Robertson <steverobertson@gmail.com>
2022-04-02 20:58:21 +0100

    Using 'let' and 'const'

--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -10,11 +10,11 @@ declare function importScripts(path: string);
 declare function postMessage(msg);
 
 // WebAssembly module cache
-var _WASM_module_cache = {};
-var CACHE_WASM_MODULES = true; // if false, use asm.js only
+const _WASM_module_cache = {};
+let CACHE_WASM_MODULES = true; // if false, use asm.js only
 
 function getWASMModule(module_id: string) {
-    var module = _WASM_module_cache[module_id];
+    let module = _WASM_module_cache[module_id];
 
     if (!module) {
         starttime();
@@ -34,21 +34,21 @@ function getWASMModule(module_id: string) {
 // function for use with instantiateWasm
 export function moduleInstFn(module_id: string) {
     return function (imports, ri) {
-        var mod = getWASMModule(module_id);
-        var inst = new WebAssembly.Instance(mod, imports);
+        const mod = getWASMModule(module_id);
+        const inst = new WebAssembly.Instance(mod, imports);
         ri(inst);
         return inst.exports;
     }
 }
 
-var _t1;
+let _t1;
 
-export function starttime() {
+function starttime() {
     _t1 = new Date();
 }
 
-export function endtime(msg) {
-    var _t2 = new Date();
+function endtime(msg) {
+    const _t2 = new Date();
     console.log(msg, _t2.getTime() - _t1.getTime(), "ms");
 }
 
@@ -56,7 +56,7 @@ export function endtime(msg) {
 
 export var store = new FileWorkingStore();
 
-var builder = new Builder();
+const builder = new Builder();
 
 export function putWorkFile(path: string, data: FileData) {
     return store.putFile(path, data);
@@ -67,15 +67,15 @@ export function getWorkFileAsString(path: string): string {
 }
 
 export function populateEntry(fs, path: string, entry: FileEntry, options: BuildOptions) {
-    var data = entry.data;
+    let data = entry.data;
     if (options && options.processFn) {
         data = options.processFn(path, data);
     }
 
     // create subfolders
-    var toks = path.split('/');
+    const toks = path.split('/');
     if (toks.length > 1) {
-        for (var i = 0; i < toks.length - 1; i++) {
+        for (let i = 0; i < toks.length - 1; i++) {
             try {
                 fs.mkdir(toks[i]);
             } catch (e) {
@@ -86,18 +86,18 @@ export function populateEntry(fs, path: string, entry: FileEntry, options: Build
 
     // write file
     fs.writeFile(path, data, {encoding: entry.encoding});
-    var time = new Date(entry.ts);
+    const time = new Date(entry.ts).getTime();
     fs.utime(path, time, time);
     console.log("<<<", path, entry.data.length);
 }
 
 // can call multiple times (from populateFiles)
 export function gatherFiles(step: BuildStep, options?: BuildOptions): number {
-    var maxts = 0;
+    let maxts = 0;
     if (step.files) {
-        for (var i = 0; i < step.files.length; i++) {
-            var path = step.files[i];
-            var entry = store.workfs[path];
+        for (let i = 0; i < step.files.length; i++) {
+            const path = step.files[i];
+            const entry = store.workfs[path];
 
             if (!entry) {
                 throw new Error("No entry for path '" + path + "'");
@@ -106,20 +106,20 @@ export function gatherFiles(step: BuildStep, options?: BuildOptions): number {
             }
         }
     } else if (step.code) {
-        var path = step.path ? step.path : options.mainFilePath; // TODO: what if options null
+        const path = step.path ? step.path : options.mainFilePath;
 
         if (!path) {
             throw Error("need path or mainFilePath");
         }
 
-        var code = step.code;
-        var entry = putWorkFile(path, code);
+        const code = step.code;
+        const entry = putWorkFile(path, code);
         step.path = path;
         step.files = [path];
         maxts = entry.ts;
     } else if (step.path) {
-        var path = step.path;
-        var entry = store.workfs[path];
+        const path = step.path;
+        const entry = store.workfs[path];
         maxts = entry.ts;
         step.files = [path];
     }
@@ -133,7 +133,7 @@ export function gatherFiles(step: BuildStep, options?: BuildOptions): number {
 }
 
 export function getPrefix(s: string): string {
-    var pos = s.lastIndexOf('.');
+    const pos = s.lastIndexOf('.');
     return (pos > 0) ? s.substring(0, pos) : s;
 }
 
@@ -144,16 +144,16 @@ export function populateFiles(step: BuildStep, fs, options?: BuildOptions) {
         throw Error("call gatherFiles() first");
     }
 
-    for (var i = 0; i < step.files.length; i++) {
-        var path = step.files[i];
+    for (let i = 0; i < step.files.length; i++) {
+        const path = step.files[i];
         populateEntry(fs, path, store.workfs[path], options);
     }
 }
 
 export function populateExtraFiles(step: BuildStep, fs, extrafiles) {
     if (extrafiles) {
-        for (var i = 0; i < extrafiles.length; i++) {
-            var xfn = extrafiles[i];
+        for (let i = 0; i < extrafiles.length; i++) {
+            const xfn = extrafiles[i];
 
             // is this file cached?
             if (store.workfs[xfn]) {
@@ -162,14 +162,14 @@ export function populateExtraFiles(step: BuildStep, fs, extrafiles) {
             }
 
             // fetch from network
-            var xpath = "zx/" + xfn;
-            var xhr = new XMLHttpRequest();
+            const xpath = "zx/" + xfn;
+            const xhr = new XMLHttpRequest();
             xhr.responseType = 'arraybuffer';
             xhr.open("GET", PWORKER + xpath, false);  // synchronous request
             xhr.send(null);
 
             if (xhr.response && xhr.status == 200) {
-                var data = new Uint8Array(xhr.response);
+                const data = new Uint8Array(xhr.response);
                 fs.writeFile(xfn, data, {encoding: 'binary'});
                 putWorkFile(xfn, data);
                 console.log(":::", xfn, data.length);
@@ -186,8 +186,8 @@ export function staleFiles(step: BuildStep, targets: string[]) {
     }
 
     // see if any target files are more recent than inputs
-    for (var i = 0; i < targets.length; i++) {
-        var entry = store.workfs[targets[i]];
+    for (let i = 0; i < targets.length; i++) {
+        const entry = store.workfs[targets[i]];
         if (!entry || step.maxts > entry.ts) {
             return true;
         }
@@ -203,8 +203,8 @@ export function anyTargetChanged(step: BuildStep, targets: string[]) {
     }
 
     // see if any target files are more recent than inputs
-    for (var i = 0; i < targets.length; i++) {
-        var entry = store.workfs[targets[i]];
+    for (let i = 0; i < targets.length; i++) {
+        const entry = store.workfs[targets[i]];
         if (!entry || entry.ts > step.maxts) {
             return true;
         }
@@ -216,36 +216,38 @@ export function anyTargetChanged(step: BuildStep, targets: string[]) {
 
 export function execMain(step: BuildStep, mod, args: string[]) {
     starttime();
-    var run = mod.callMain || mod.run;
+    const run = mod.callMain || mod.run;
     run(args);
     endtime(step.tool);
 }
 
 /// asm.js / WASM / filesystem loading
 
-var fsMeta = {};
-var fsBlob = {};
-var wasmBlob = {};
+const fsMeta = {};
+const fsBlob = {};
+const wasmBlob = {};
 
 const PSRC = "../";
 const PWORKER = PSRC + "worker/";
 
 // load filesystems for CC65 and others asynchronously
 function loadFilesystem(name: string) {
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.responseType = 'blob';
     xhr.open("GET", PWORKER + "fs/fs" + name + ".data", false);  // synchronous request
     xhr.send(null);
     fsBlob[name] = xhr.response;
+
     xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.open("GET", PWORKER + "fs/fs" + name + ".js.metadata", false);  // synchronous request
     xhr.send(null);
     fsMeta[name] = xhr.response;
+
     console.log("Loaded " + name + " filesystem", fsMeta[name].files.length, 'files', fsBlob[name].size, 'bytes');
 }
 
-var loaded = {};
+const loaded = {};
 
 export function load(modulename: string, debug?: boolean) {
     if (!loaded[modulename]) {
@@ -257,7 +259,8 @@ export function load(modulename: string, debug?: boolean) {
 export function loadWASM(modulename: string, debug?: boolean) {
     if (!loaded[modulename]) {
         importScripts(PWORKER + "wasm/" + modulename + (debug ? "." + debug + ".js" : ".js"));
-        var xhr = new XMLHttpRequest();
+
+        const xhr = new XMLHttpRequest();
         xhr.responseType = 'arraybuffer';
         xhr.open("GET", PWORKER + "wasm/" + modulename + ".wasm", false);  // synchronous request
         xhr.send(null);
@@ -283,7 +286,7 @@ export function loadNative(modulename: string) {
 
 // mount the filesystem at /share
 export function setupFS(FS, name: string) {
-    var WORKERFS = FS.filesystems['WORKERFS'];
+    const WORKERFS = FS.filesystems['WORKERFS'];
 
     if (!fsMeta[name]) {
         throw Error("No filesystem for '" + name + "'");
@@ -298,17 +301,17 @@ export function setupFS(FS, name: string) {
     // https://github.com/kripken/emscripten/blob/incoming/src/library_workerfs.js
     // https://bugs.chromium.org/p/chromium/issues/detail?id=349304#c30
 
-    var reader = WORKERFS.reader;
-    var blobcache = {};
+    const reader = WORKERFS.reader;
+    const blobcache = {};
 
     WORKERFS.stream_ops.read = function (stream, buffer, offset, length, position) {
         if (position >= stream.node.size) {
             return 0;
         }
 
-        var contents = blobcache[stream.path];
+        let contents = blobcache[stream.path];
         if (!contents) {
-            var ab = reader.readAsArrayBuffer(stream.node.contents);
+            const ab = reader.readAsArrayBuffer(stream.node.contents);
             contents = blobcache[stream.path] = new Uint8Array(ab);
         }
 
@@ -316,7 +319,7 @@ export function setupFS(FS, name: string) {
             length = contents.length - position;
         }
 
-        for (var i = 0; i < length; i++) {
+        for (let i = 0; i < length; i++) {
             buffer[offset + i] = contents[position + i];
         }
 
@@ -336,9 +339,9 @@ export const re_msvc2 = /\s*(at)\s+(\d+)\s*(:)\s*(.*)/;
 
 export function msvcErrorMatcher(errors: WorkerError[]) {
     return function (s: string) {
-        var matches = re_msvc.exec(s) || re_msvc2.exec(s);
+        const matches = re_msvc.exec(s) || re_msvc2.exec(s);
         if (matches) {
-            var errline = parseInt(matches[2]);
+            const errline = parseInt(matches[2]);
             errors.push({
                 line: errline,
                 path: matches[1],
@@ -352,7 +355,7 @@ export function msvcErrorMatcher(errors: WorkerError[]) {
 
 export function makeErrorMatcher(errors: WorkerError[], regex, iline: number, imsg: number, mainpath: string, ifilename?: number) {
     return function (s) {
-        var matches = regex.exec(s);
+        const matches = regex.exec(s);
         if (matches) {
             errors.push({
                 line: parseInt(matches[iline]) || 1,
@@ -366,10 +369,10 @@ export function makeErrorMatcher(errors: WorkerError[], regex, iline: number, im
 }
 
 export function extractErrors(regex, strings: string[], path: string, iline, imsg, ifilename) {
-    var errors = [];
-    var matcher = makeErrorMatcher(errors, regex, iline, imsg, path, ifilename);
+    const errors = [];
+    const matcher = makeErrorMatcher(errors, regex, iline, imsg, path, ifilename);
 
-    for (var i = 0; i < strings.length; i++) {
+    for (let i = 0; i < strings.length; i++) {
         matcher(strings[i]);
     }
 
@@ -383,11 +386,12 @@ export function parseListing(code: string,
                              lineMatch, iline: number, ioffset: number, iinsns: number, icycles?: number,
                              funcMatch?, segMatch?): SourceLine[] {
 
-    var lines: SourceLine[] = [];
-    var lineofs = 0;
-    var segment = '';
-    var func = '';
-    var funcbase = 0;
+    const lines: SourceLine[] = [];
+
+    let lineofs = 0;
+    let segment = '';
+    let func = '';
+    let funcbase = 0;
 
     code.split(re_crlf).forEach((line, lineindex) => {
         let segm = segMatch && segMatch.exec(line);
@@ -401,13 +405,13 @@ export function parseListing(code: string,
             func = funcm[2];
         }
 
-        var linem = lineMatch.exec(line);
+        const linem = lineMatch.exec(line);
         if (linem && linem[1]) {
-            var linenum = iline < 0 ? lineindex : parseInt(linem[iline]);
-            var offset = parseInt(linem[ioffset], 16);
-            var insns = linem[iinsns];
-            var cycles: number = icycles ? parseInt(linem[icycles]) : null;
-            var iscode = cycles > 0;
+            const linenum = iline < 0 ? lineindex : parseInt(linem[iline]);
+            const offset = parseInt(linem[ioffset], 16);
+            const insns = linem[iinsns];
+            const cycles: number = icycles ? parseInt(linem[icycles]) : null;
+            const iscode = cycles > 0;
 
             if (insns) {
                 lines.push({
@@ -432,13 +436,14 @@ export function parseListing(code: string,
 }
 
 export function parseSourceLines(code: string, lineMatch, offsetMatch, funcMatch?, segMatch?) {
-    var lines = [];
-    var lastlinenum = 0;
-    var segment = '';
-    var func = '';
-    var funcbase = 0;
+    const lines = [];
 
-    for (var line of code.split(re_crlf)) {
+    let lastlinenum = 0;
+    let segment = '';
+    let func = '';
+    let funcbase = 0;
+
+    for (let line of code.split(re_crlf)) {
         let segm = segMatch && segMatch.exec(line);
         if (segm) {
             segment = segm[1];
@@ -450,13 +455,13 @@ export function parseSourceLines(code: string, lineMatch, offsetMatch, funcMatch
             func = funcm[2];
         }
 
-        var linem = lineMatch.exec(line);
+        let linem = lineMatch.exec(line);
         if (linem && linem[1]) {
             lastlinenum = parseInt(linem[1]);
         } else if (lastlinenum) {
-            var linem = offsetMatch.exec(line);
+            linem = offsetMatch.exec(line);
             if (linem && linem[1]) {
-                var offset = parseInt(linem[1], 16);
+                const offset = parseInt(linem[1], 16);
 
                 lines.push({
                     line: lastlinenum,
@@ -469,11 +474,12 @@ export function parseSourceLines(code: string, lineMatch, offsetMatch, funcMatch
             }
         }
     }
+
     return lines;
 }
 
 export function setupStdin(fs, code: string) {
-    var i = 0;
+    let i = 0;
     fs.init(
         function () {
             return i < code.length ? code.charCodeAt(i++) : null;
@@ -488,24 +494,24 @@ function makeCPPSafe(s: string): string {
 export function preprocessMCPP(step: BuildStep, filesys: string) {
     load("mcpp");
 
-    var platform = step.platform;
-    var params = PLATFORM_PARAMS['zx'];
+    const platform = step.platform;
+    const params = PLATFORM_PARAMS['zx'];
 
     if (!params) {
         throw Error("Platform not supported: " + platform);
     }
 
     // <stdin>:2: error: Can't open include file "foo.h"
-    var errors = [];
-    var match_fn = makeErrorMatcher(errors, /<stdin>:(\d+): (.+)/, 1, 2, step.path);
-    var MCPP: EmscriptenModule = emglobal.mcpp({
+    let errors = [];
+    const match_fn = makeErrorMatcher(errors, /<stdin>:(\d+): (.+)/, 1, 2, step.path);
+    const MCPP: EmscriptenModule = emglobal.mcpp({
         noInitialRun: true,
         noFSInit: true,
         print: print_fn,
         printErr: match_fn,
     });
 
-    var FS = MCPP.FS;
+    const FS = MCPP.FS;
 
     if (filesys) {
         setupFS(FS, filesys);
@@ -513,7 +519,7 @@ export function preprocessMCPP(step: BuildStep, filesys: string) {
 
     populateFiles(step, FS);
 
-    var args = [
+    const args = [
         "-D", "__8BITWORKSHOP__",
         "-D", "__SDCC_z80",
         "-D", makeCPPSafe(platform.toUpperCase()),
@@ -528,24 +534,23 @@ export function preprocessMCPP(step: BuildStep, filesys: string) {
     execMain(step, MCPP, args);
 
     if (errors.length) {
-        return {errors: errors};
+        return {errors};
     }
 
-    var iout = FS.readFile("main.i", {encoding: 'utf8'});
+    let iout = FS.readFile("main.i", {encoding: 'utf8'});
     iout = iout.replace(/^#line /gm, '\n# ');
 
     try {
-        var errout = FS.readFile("mcpp.err", {encoding: 'utf8'});
+        const errout = FS.readFile("mcpp.err", {encoding: 'utf8'});
         if (errout.length) {
 
             // //main.c:2: error: Can't open include file "stdiosd.h"
-            var errors = extractErrors(/([^:]+):(\d+): (.+)/, errout.split("\n"), step.path, 2, 3, 1);
-
+            errors = extractErrors(/([^:]+):(\d+): (.+)/, errout.split("\n"), step.path, 2, 3, 1);
             if (errors.length == 0) {
                 errors = errorResult(errout).errors;
             }
 
-            return {errors: errors};
+            return {errors};
         }
     } catch (e) {
         console.error(e);
@@ -558,7 +563,7 @@ async function handleMessage(data: WorkerMessage): Promise<WorkerResult> {
 
     // preload file system
     if (data.preload) {
-        var fs = TOOL_PRELOADFS[data.preload];
+        let fs = TOOL_PRELOADFS[data.preload];
 
         if (!fs && data.platform) {
             fs = TOOL_PRELOADFS[data.preload + '-zx'];
@@ -584,12 +589,12 @@ async function handleMessage(data: WorkerMessage): Promise<WorkerResult> {
     return builder.handleMessage(data);
 }
 
-var lastpromise = null;
+let lastpromise = null;
 
 onmessage = async function (e) {
     await lastpromise; // wait for previous message to complete
     lastpromise = handleMessage(e.data);
-    var result = await lastpromise;
+    const result = await lastpromise;
     lastpromise = null;
 
     if (result) {
