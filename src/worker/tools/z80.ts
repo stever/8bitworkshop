aa30dad9f3168a5c3d875f6eee9d221f7aaa1cf0
Steven Robertson <steverobertson@gmail.com>
2022-04-03 11:37:05 +0100

    Code-style updates for z80.ts

--- src/worker/tools/z80.ts
+++ src/worker/tools/z80.ts
@@ -16,16 +16,16 @@ import {print_fn, execMain} from "../util";
 export function assembleZMAC(step: BuildStep): BuildStepResult {
     loadNative("zmac");
 
-    var lstout, binout;
-    var errors = [];
+    let lstout, binout;
+    const errors = [];
 
     gatherFiles(step, {mainFilePath: "main.asm"});
 
-    var lstpath = step.prefix + ".lst";
-    var binpath = step.prefix + ".cim";
+    const lstpath = step.prefix + ".lst";
+    const binpath = step.prefix + ".cim";
 
     if (staleFiles(step, [binpath, lstpath])) {
-        var ZMAC: EmscriptenModule = emglobal.zmac({
+        const ZMAC: EmscriptenModule = emglobal.zmac({
             instantiateWasm: moduleInstFn('zmac'),
             noInitialRun: true,
             //logReadFiles:true,
@@ -33,7 +33,7 @@ export function assembleZMAC(step: BuildStep): BuildStepResult {
             printErr: makeErrorMatcher(errors, /([^( ]+)\s*[(](\d+)[)]\s*:\s*(.+)/, 2, 3, step.path),
         });
 
-        var FS = ZMAC.FS;
+        const FS = ZMAC.FS;
         populateFiles(step, FS);
         execMain(step, ZMAC, ['-z', '-c', '--oo', 'lst,cim', step.path]);
 
@@ -52,17 +52,17 @@ export function assembleZMAC(step: BuildStep): BuildStepResult {
         }
 
         //  230: 1739+7+x   017A  1600      L017A: LD      D,00h
-        var lines = parseListing(lstout, /\s*(\d+):\s*([0-9a-f]+)\s+([0-9a-f]+)\s+(.+)/i, 1, 2, 3);
-        var listings: CodeListingMap = {};
+        const lines = parseListing(lstout, /\s*(\d+):\s*([0-9a-f]+)\s+([0-9a-f]+)\s+(.+)/i, 1, 2, 3);
+        const listings: CodeListingMap = {};
         listings[lstpath] = {lines: lines};
 
         // parse symbol table
-        var symbolmap = {};
-        var sympos = lstout.indexOf('Symbol Table:');
+        const symbolmap = {};
+        const sympos = lstout.indexOf('Symbol Table:');
         if (sympos > 0) {
-            var symout = lstout.slice(sympos + 14);
+            const symout = lstout.slice(sympos + 14);
             symout.split('\n').forEach(function (l) {
-                var m = l.match(/(\S+)\s+([= ]*)([0-9a-f]+)/i);
+                const m = l.match(/(\S+)\s+([= ]*)([0-9a-f]+)/i);
                 if (m) {
                     symbolmap[m[1]] = parseInt(m[3], 16);
                 }
