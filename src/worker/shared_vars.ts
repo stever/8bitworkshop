3187a7ef8d8e8a10fe264779f87d5f15657d719a
Steven Robertson <steverobertson@gmail.com>
2022-04-04 11:25:21 +0100

    Renaming the worker public folder

--- src/worker/shared_vars.ts
+++ src/worker/shared_vars.ts
@@ -4,4 +4,4 @@ const ENVIRONMENT_IS_WEB = typeof window === 'object';
 const ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
 export const emglobal: any = ENVIRONMENT_IS_WORKER ? self : ENVIRONMENT_IS_WEB ? window : global;
 
-export const WORKER_RELATIVE_PATH = "../worker/";
+export const WORKER_RELATIVE_PATH = "../8bitworker/";
